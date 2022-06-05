42a80b3e4604f1c2df2c6b2cfcd8277fb538ce7e
snnrsnnr <104438205+snnrsnnr@users.noreply.github.com>
2022-06-05 21:42:55 +0300

    Proje

--- Mystic Flight/Assets/Scripts/GameManager.cs
+++ Mystic Flight/Assets/Scripts/GameManager.cs
@@ -4,30 +4,45 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class GameManager : MonoBehaviour
 {
+    
+
     public int score;
+    public int highScore;
 
     public Text ScoreText;
 
+    public Text HighScore;
+
+    
     void Start()
     {
-        score = 0;
-        ScoreText.text = score.ToString();
-    }
-   
+        highScore = PlayerPrefs.GetInt("highscore");
+        HighScore.text = highScore.ToString();
 
+        ScoreText.text = score.ToString();
+       
+        
+    }
+    
     void Update()
     {
         
     }
-   public void UpdateScore()
+    public void UpdateScore()
     {
         score++;
         ScoreText.text = score.ToString();
 
+        if (score > highScore)
+        {
+            highScore = score;
+            HighScore.text = highScore.ToString();
+            PlayerPrefs.SetInt("highscore", highScore);
+        }
     }
-    
     public void RestartGame()
     {
         SceneManager.LoadScene(1);
@@ -38,5 +53,6 @@ public class GameManager : MonoBehaviour
         Debug.Log("Oyundan Çýktýk");
         Application.Quit();
     }
+
 
 }
